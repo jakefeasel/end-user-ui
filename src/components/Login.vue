e69dddb281f79a16212f910fe87bddea70b18105
Jake Feasel <jake.feasel@forgerock.com>
2019-01-29 10:42:03 -0800

    Prototype of embeddedLogin usage

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,24 +1,8 @@
 <template>
     <fr-center-card :showLogo="true">
-        <div slot="center-card-header">
-            <h2 class="h2">{{pageData.header}}</h2>
-        </div>
-
         <b-card-body slot="center-card-body">
-            <b-form  v-if="pageData.callbacks" class="form-signin mb-3" @submit.prevent="submit">
-                <template  v-for="(callback) in pageData.callbacks">
-                    <template  v-for="(input, index) in callback.input">
-                        <fr-floating-label-input v-if="callback.type === 'PasswordCallback'" v-model="input.value" :fieldName="callback.output[index].value" :label="callback.output[index].value" type="password" autofocus="true"></fr-floating-label-input>
-
-                        <fr-floating-label-input v-else v-model="input.value" :fieldName="callback.output[index].value" :label="callback.output[index].value" type="text" autofocus="true"></fr-floating-label-input>
-                    </template>
-                </template>
-                <b-button type="submit" variant="primary" class="btn btn-block btn-lg">
-                    {{$t('pages.login.signIn')}}
-                </b-button>
+            <b-form class="form-signin mb-3" @submit.prevent="submit">
             </b-form>
-
-            <bounce-loader v-else :color="loadingColor"></bounce-loader>
         </b-card-body>
     </fr-center-card>
 </template>
@@ -26,28 +10,16 @@
 <script>
     import FloatingLabelInput from '@/components/utils/FloatingLabelInput';
     import CenterCard from '@/components/utils/CenterCard';
-    import { BounceLoader } from 'vue-spinner/dist/vue-spinner.min.js';
     import styles from '@/scss/main.scss';
     import ForgeRockEmbeddedLogin from 'forgerockembeddedlogin';
 
     var login = new ForgeRockEmbeddedLogin('https://login.sample.forgeops.com/json/realms/root/authenticate');
 
-    /**
-     * @description Controlling component to allow users to manually login, socially login or start of a selfservice process (username, password or registration) if configured.
-     *
-     * @fires POST authentication?_action=logout - Ends current user session
-     * @fires POST authentication?_action=login - Uses username and password to establish a new user session
-     * @fires GET type/name/id (e.g. managed/user/_id) - Resource details, in this context it is for the successfully logged in user
-     * @fires POST privilege?_action=listPrivileges - Check to see if a user has any privilege based access
-     * @fires GET schema/type/name/ (e.g. schema/managed/user) - Schema for a resource
-     *
-     */
     export default {
         name: 'Login',
         components: {
             'fr-floating-label-input': FloatingLabelInput,
-            'fr-center-card': CenterCard,
-            BounceLoader
+            'fr-center-card': CenterCard
         },
         data () {
             return {
@@ -57,23 +29,27 @@
             };
         },
         created () {
-            this.loadData();
+            login.startLogin()
+            .then(this.handleLoginResponse);
         },
         methods: {
-            handleCallbacks (callbacks) {
+            handleLoginResponse () {
                 if (login.success()) {
                     this.completeLogin();
+                } else if (login.failure()) {
+                    this.displayNotification('error', 'Login Failed! Try Again');
+                    login.startLogin().then(this.render);
                 } else {
-                    this.pageData = callbacks;
+                    this.render();
                 }
             },
-            loadData () {
-                login.startLogin()
-                .then(this.handleCallbacks);
+            render () {
+                login.renderLogin().then((loginPrompt) => {
+                    document.getElementsByClassName('form-signin mb-3')[0].innerHTML = loginPrompt;
+                });
             },
-            submit () {
-                return login.submitCredentials(this.pageData.callbacks.map((callback) => callback.input[0].value))
-                .then(this.handleCallbacks);
+            submit (e) {
+                return login.handleLoginSubmit(e).then(this.handleLoginResponse);
             }
         }
     };
